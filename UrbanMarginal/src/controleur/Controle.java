31bae617cdc8bd360f2e960de77bc776c8b6ee9e
gaelle <gaelle@pc>
2016-09-15 18:28:46 +0200

    re install git

---
+++ UrbanMarginal/src/controleur/Controle.java
@@ -0,0 +1,44 @@
+package controleur;
+import vue.EntreeJeu;
+import java.util.*;
+
+import javax.swing.JFrame;
+public class Controle {
+	private EntreeJeu  frmEntreeJeu ;
+	
+	public static void main(String[] args) {
+		new Controle ();
+		
+	}
+	// Constructeur 
+	public Controle (){
+		this.frmEntreeJeu = new EntreeJeu(this); // 
+		frmEntreeJeu.setVisible(true);
+		
+	}
+	public void evenementVue (JFrame uneFrame ,Object info ) { // methode qui recoit en param uneframe de type Jframe et info de type objet 
+		if (uneFrame instanceof EntreeJeu){
+			evenementEntreeJeu (info);
+		}
+		
+	}
+	private void evenementEntreeJeu(Object info) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
