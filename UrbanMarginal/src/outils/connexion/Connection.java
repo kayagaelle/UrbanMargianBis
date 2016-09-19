9c5629bb803e75607d967a07f7b68d4765348ca2
gaelle <gaelle@10.20.129.66>
2016-09-19 12:58:20 +0200

    méthode run de la page 19

--- UrbanMarginal/src/outils/connexion/Connection.java
+++ UrbanMarginal/src/outils/connexion/Connection.java
@@ -1,6 +1,9 @@
 package outils.connexion;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Connection extends Thread {
@@ -8,9 +11,9 @@ public class Connection extends Thread {
 	//Déclaration de propriétés 
 	
 	private Object leRecepteur ;
-	private ObjectOutputStream In ;
-	private ObjectOutputStream Out ;
-	
+	private ObjectInputStream in ;
+	private ObjectOutputStream out ;
+	public boolean inOk = true;
 	
 	
 	// Consttructeur de la classe Connection
@@ -19,6 +22,26 @@ public class Connection extends Thread {
 		//valorisation de la propriété leRecpeteur dans le constructeur
 		this.leRecepteur=leRecepteur ;
 		
+		try {
+			out = new ObjectOutputStream (socket.getOutputStream()) ;
+		} catch (IOException e) {
+			System.out.println("Erreur de socket  : "+e);
+			System.exit(0);
+		}
+		
+		try {
+			in = new ObjectInputStream (socket.getInputStream()) ;
+		} catch (IOException e) {
+			System.out.println("Erreur de socket  : "+e);
+			System.exit(0);
+		}
+		super.start();
+		
+		}
+	
+		public void run (boolean inOk ){
+			
+		}
 		
 		
 		
@@ -26,6 +49,12 @@ public class Connection extends Thread {
 		
 		
 		
+		
+	
+
+
+
+	
 	}
 	
 	
@@ -48,4 +77,4 @@ public class Connection extends Thread {
 	
 	
 	
-}
+
