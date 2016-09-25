849ae80da1627f95fd1955bb500ac845d494c04f
gaelle <gaelle@pc>
2016-09-26 00:55:48 +0200

    affichage marche , debut page 25

--- UrbanMarginal/src/vue/ChoixJoueur.java
+++ UrbanMarginal/src/vue/ChoixJoueur.java
@@ -3,9 +3,11 @@ package vue;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class ChoixJoueur extends JFrame {
 
@@ -19,11 +21,17 @@ public class ChoixJoueur extends JFrame {
 	public ChoixJoueur() {
 		setTitle("Choice");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 416, 313);
 		contentPane = new JPanel();
+	
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblFond = new JLabel("");
+		lblFond.setBounds(0, 0, 400, 275);
+		lblFond.setIcon(new ImageIcon ("media/fonds/fondchoix.jpg"));
+		contentPane.add(lblFond);
+		
 	}
-
 }
