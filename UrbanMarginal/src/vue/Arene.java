44a13e1c54f7b4cc2b025a861060c9adf1c54d43
gaelle <gaelle@pc>
2016-09-30 23:47:07 +0200

    Le fond est mit , fin page 33

--- UrbanMarginal/src/vue/Arene.java
+++ UrbanMarginal/src/vue/Arene.java
@@ -3,15 +3,19 @@ package vue;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controleur.Global;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 
-public class Arene extends JFrame {
+public class Arene extends JFrame implements Global {
 
 	private JPanel contentPane;
 	private JTextField txtSaisie;
@@ -22,9 +26,10 @@ public class Arene extends JFrame {
 	 * Create the frame.
 	 */
 	public Arene() {
+		
 		setTitle("Arena");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 616, 414);
+		setBounds(100, 100, L_ARENE+3*MARGE,H_ARENE + H_CHAT);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -34,24 +39,29 @@ public class Arene extends JFrame {
 		jpnJeu.setBounds(459, 42, 87, 85);
 		contentPane.add(jpnJeu);
 		jpnJeu.setLayout(null);
+		jpnJeu.setOpaque(false);
 		
 		JPanel jpnMurs = new JPanel();
 		jpnMurs.setBounds(273, 181, 273, 73);
 		contentPane.add(jpnMurs);
 		jpnMurs.setLayout(null);
+		jpnMurs.setOpaque(false);
+		
 		
 		JLabel lblFond = new JLabel("");
-		lblFond.setBounds(10, 11, 321, 158);
+		lblFond.setBounds(0, 0, L_ARENE, H_ARENE);
 		contentPane.add(lblFond);
+		lblFond.setIcon(new ImageIcon(FONDARENE));
 		
 		txtSaisie = new JTextField();
-		txtSaisie.setBounds(10, 271, 536, 20);
+		txtSaisie.setBounds(0, H_ARENE, L_ARENE, H_SAISIE);
 		contentPane.add(txtSaisie);
 		txtSaisie.setColumns(10);
 		
 		JScrollPane jspChat = new JScrollPane();
-		jspChat.setBounds(10, 302, 536, 62);
+		jspChat.setBounds(0, H_ARENE+H_SAISIE, L_ARENE, H_CHAT-H_SAISIE-7*MARGE);
 		contentPane.add(jspChat);
+		jspChat.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		JTextArea txtChat = new JTextArea();
 		jspChat.setViewportView(txtChat);
