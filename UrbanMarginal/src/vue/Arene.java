1fd414b956525aeaf52dad510f0129cf01cbbf54
gaelle <gaelle@pc>
2016-09-30 23:33:37 +0200

    Installation des JPanel , txtArea...

--- UrbanMarginal/src/vue/Arene.java
+++ UrbanMarginal/src/vue/Arene.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 public class Arene extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtSaisie;
 
 	
 
@@ -19,11 +24,36 @@ public class Arene extends JFrame {
 	public Arene() {
 		setTitle("Arena");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 616, 414);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JPanel jpnJeu = new JPanel();
+		jpnJeu.setBounds(459, 42, 87, 85);
+		contentPane.add(jpnJeu);
+		jpnJeu.setLayout(null);
+		
+		JPanel jpnMurs = new JPanel();
+		jpnMurs.setBounds(273, 181, 273, 73);
+		contentPane.add(jpnMurs);
+		jpnMurs.setLayout(null);
+		
+		JLabel lblFond = new JLabel("");
+		lblFond.setBounds(10, 11, 321, 158);
+		contentPane.add(lblFond);
+		
+		txtSaisie = new JTextField();
+		txtSaisie.setBounds(10, 271, 536, 20);
+		contentPane.add(txtSaisie);
+		txtSaisie.setColumns(10);
+		
+		JScrollPane jspChat = new JScrollPane();
+		jspChat.setBounds(10, 302, 536, 62);
+		contentPane.add(jspChat);
+		
+		JTextArea txtChat = new JTextArea();
+		jspChat.setViewportView(txtChat);
 	}
-
 }
